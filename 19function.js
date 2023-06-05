9ff9b21e10e1fa80e0e6ebf6ff6333c1187668ba
Uygar Muntaş <muntasuygar@gmail.com>
2023-06-05 23:22:33 +0300

    break , continue and functions

---
+++ 19function.js
@@ -0,0 +1,11 @@
+function startProgram() {
+  let userName = "Bro";
+  let age = 21;
+
+  happyBirthday(userName, age);
+}
+function happyBirthday(name, age) {
+  console.log("hello !", name);
+  console.log("you are", age, "years old");
+}
+startProgram();
